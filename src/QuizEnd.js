88036f0d548e32ac07479c2a903252f22f79f71a
reacThot <58120866+reacThot@users.noreply.github.com>
2019-11-23 17:41:51 -0600

    Cambio 04

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,15 +1,7 @@
 import React, {Component} from 'react'
 
 class QuizEnd extends Component{
-  render{
-    return(
-      <div>
-        <p>Tranks for playing!</p>
-        <a href='#'>Reset Quiz</p>
-      </div>
-    )
-  }
-
+  
 }
 
 export default QuizEnd
