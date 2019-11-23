69421ed7e1c03ae7e5f5208717bfe791d7652ae9
reacThot <58120866+reacThot@users.noreply.github.com>
2019-11-23 17:39:38 -0600

    Cange 01

---
+++ src/QuizEnd.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class QuizEnd extends Component{
+  render{
+    return(
+      <div>
+        <p>Tranks for playing!</p>
+        <a href='#'>Reset Quiz</p>
+      </div>
+    )
+  }
+
+}
+
+export defaukt QuizEnd
