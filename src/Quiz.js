eab340603ac24953be89d9055ffe8f0104b207d9
reacThot <58120866+reacThot@users.noreply.github.com>
2019-11-23 22:56:18 -0600

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,13 +15,17 @@ class Quiz extends Component{
         return({quiz_position : state.quiz_position + 1})
         });
     }
+    
+    handleResetClick() {
+        
+    }
 
     render(){
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
 
         return (<div>
             {isQuizEnd 
-                ? <QuizEnd />
+                ? <QuizEnd resetClicHandlres={this.handleResetClick.bind(this)} />
             : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
         </div>)
     }
