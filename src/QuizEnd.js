e4476e0746ce40df3f1f4b14fff7629e1272e859
reacThot <58120866+reacThot@users.noreply.github.com>
2019-11-23 18:13:28 -0600

    change03

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,6 +1,12 @@
 import React, { Component } form 'react'
 
 class QuizEnd extends Component{
+  render() {
+    <div>
+      <p>Thanks for playing</p>
+      <a href='#'>Reset Quiz</p>
+    </div>
+  }
   
 }
 
