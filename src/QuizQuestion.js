be658b4e1b52b76b616ef7d263f7ca1ed0af8587
reacThot <58120866+reacThot@users.noreply.github.com>
2019-11-23 22:12:02 -0600

    Update QuizQuestion.js

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
+
 class QuizQuestion extends Component{
   render() {
     return(
@@ -9,13 +10,15 @@ class QuizQuestion extends Component{
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+            {this.props.quiz_question.answer_options.map((answer_option, index) => {
+              return <QuizQuestionButton key={index} button_text={answer_option}/>
+            })}
+    
           </ul>
         </section>
       </main>
     )
   }
-
 }
 
 export default QuizQuestion
