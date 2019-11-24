3ac5386b6b27828a633640856f21e7a3aa466c16
reacThot <58120866+reacThot@users.noreply.github.com>
2019-11-23 22:48:51 -0600

    cambio 04

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -32,7 +32,7 @@ class QuizQuestion extends Component{
     
           </ul>
         </section>
-            {this.state.incorrectAnswer ? <p clasName='error'>Sorry, that's not right</p> : null}
+            {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
       </main>
     )
   }
