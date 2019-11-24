3b5cdcec255664a8dfb3897ceb4107c4b8d36042
reacThot <58120866+reacThot@users.noreply.github.com>
2019-11-23 18:35:15 -0600

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,6 +12,7 @@ class Quiz extends Component {
   render() {
     return ( 
       <div>
+        <QuizEnd />
         <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}/>
       </div>
     )
