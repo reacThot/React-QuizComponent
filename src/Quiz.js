8c3be1a67fd19b1614892242f24e418763a47de7
reacThot <58120866+reacThot@users.noreply.github.com>
2019-11-23 22:22:30 -0600

    noche 02

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,6 +9,10 @@ class Quiz extends Component{
         super(props);
         this.state = {quiz_position: 1};
     }
+    
+    showNextQuestion(){
+        
+    }
 
     render(){
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
@@ -16,7 +20,7 @@ class Quiz extends Component{
         return (<div>
             {isQuizEnd 
                 ? <QuizEnd />
-            : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+            : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
         </div>)
     }
 }
