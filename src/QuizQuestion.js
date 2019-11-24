930a212c726e6e1a6c627e6f991a58cbdc2bddee
reacThot <58120866+reacThot@users.noreply.github.com>
2019-11-23 22:35:54 -0600

    noche 04

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,9 +2,19 @@ import React, {Component} from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component{
+    constructor(props){
+        super(props)
+        this.state = {incorrectAnswer:false}
+  }
+    
     handleClick(buttonText){
     if( buttonText === this.props.quiz_question.answer ){
-      this.props.showNextQuestionHandler();
+        this.setState({ incorrectAnswer: false})
+        this.props.showNextQuestionHandler();
+    }
+    else
+    {
+         this.setState({ incorrectAnswer: true})
     }
   }
   
@@ -22,6 +32,7 @@ class QuizQuestion extends Component{
     
           </ul>
         </section>
+      {this.setState.incorrectAnswer? <p clasName='error'>Sorry, that's not right</p> : null}
       </main>
     )
   }
